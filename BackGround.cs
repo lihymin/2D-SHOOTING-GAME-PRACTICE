e938bd7071542b33e3a1474a939296b8dd76e7e5
lihymin <sung787292@gmail.com>
2024-08-14 13:37:36 +0900

    무한 배경 로직 수정2

--- BackGround.cs
+++ BackGround.cs
@@ -32,6 +32,7 @@ public class BackGround : MonoBehaviour
     void Scrolling()
     {
         if (sprites[endIndex].position.y < viewHeight * (-1)) 
+        {
             Vector3 backSpritePos = sprites[startIndex].localPosition;
             Vector3 frontSpritePos = sprites[endIndex].localPosition;
             sprites[endIndex].transform.localPosition = backSpritePos + Vector3.up * viewHeight;
@@ -39,5 +40,6 @@ public class BackGround : MonoBehaviour
             int startIndexSave = startIndex;
             startIndex = endIndex;
             endIndex = (startIndexSave - 1 == -1) ? sprites.Length - 1 : startIndexSave - 1;
+        }
     }
 }
